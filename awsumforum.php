7bf9cb9bbc40abc7d512c56d55b80d633af5fea6
E <contact@ennarissanen.com>
2018-04-18 09:45:51 +0300

    KT4

---
+++ awsumforum.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>awsum forum</title>
+  </head>
+  <body>
+    <form action="" method="post">
+      <label>How many lines of stars?</label>
+      <input type="number" name="userSubmittedNumber">
+      <button type="submit">Do it</button>
+    </form>
+  </body>
+</html>
