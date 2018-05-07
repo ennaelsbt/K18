c5c174dcaa4c6ee0ffabcde7850ec69a18dd87fb
E <contact@ennarissanen.com>
2018-05-07 17:40:08 +0300

    Saves path to database and shows uploaded images, required fields to form

--- gallery-test.php
+++ gallery-test.php
@@ -6,8 +6,8 @@
 <body>
 
     <form action="save.php" enctype="multipart/form-data" method="POST">
-      <input type="text" name="imageName" placeholder="Name your image" />
-      <input type="file" name="submittedImage" />
+      <input type="text" name="imageName" placeholder="Name your image" required />
+      <input type="file" name="submittedImage" required />
       <input type="submit" name="submit" value="Upload" />
     </form>
 
--- save.php
+++ save.php
@@ -1,37 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Gallery</title>
+</head>
+<body>
+<h1>Here's your picture gallery!</h1>
 <?php
 
 $uploadDir = 'uploads/';
 $uploadFile = $uploadDir . basename($_FILES['submittedImage']['name']);
+
+if (!move_uploaded_file($_FILES['submittedImage']['tmp_name'], $uploadFile)) {
+    echo "Possible file upload attack!\n";
+}
 
 $imageName = $_POST['imageName'];
 $imageFilename = $_FILES['submittedImage']['name'];
 $imageType = $_FILES['submittedImage']['type'];
 $imageSize = $_FILES['submittedImage']['size'];
 
-$servername = "localhost:8889";
-$username = "root";
-$password = "root";
-$dbname = "gallery";
-$connection = new mysqli($servername, $username, $password, $dbname);
-  if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-  }
+$connection = new mysqli("localhost:8889", "root", "root", "gallery");
+if ($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
+}
 
-$uploadQuery = "INSERT INTO pictures (imageName, filename, type, size)
-VALUES ('$imageName', '$imageFilename', '$imageType', '$imageSize')";
+$uploadQuery = "INSERT INTO pictures (imageName, filename, type, path, size)
+VALUES ('$imageName', '$imageFilename', '$imageType', '$uploadFile', '$imageSize')";
 
 if ($connection->query($uploadQuery) === TRUE) {
-  $queryResult = mysqli_query($connection, $uploadQuery)
-    or die("Virhe: " . mysqli_error($connection));
-  }
-  // $queryResult = mysql_query($uploadQuery) or die(mysql_error());
-  $connection->close();
-
-if (move_uploaded_file($_FILES['submittedImage']['tmp_name'], $uploadFile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
+  echo "Your picture was uploaded successfully.";
 }
-echo 'Here is some more debugging info:';
-print_r($_FILES);
+
+$getPictures = "select filename from pictures";
+$queryResult = mysqli_query($connection, $getPictures)
+or die("Virhe: " . mysqli_error($connection));
+  while($tableRow=mysqli_fetch_array($queryResult)) {
+    echo '<img src="uploads/' . $tableRow["filename"] . '">';
+  } $connection->close();
+
+
 ?>
+
+</body>
+</html>
